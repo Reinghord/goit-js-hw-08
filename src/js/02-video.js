9629d7ec37e3dfbeefaff4a821d096b80f29bea8
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-11 14:36:15 +0300

    finished hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: '236203659',
+  width: 1024,
+});
+
+const storeCurrentTime = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(storeCurrentTime, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const { form, email, message } = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[type="email"]'),
+  message: document.querySelector('textarea'),
+};
+
+const storageObject = {};
+const onInputStoreData = function (e) {
+  storageObject[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageObject));
+};
+
+form.addEventListener('input', throttle(onInputStoreData, 500));
+
+const getStorageObject = localStorage.getItem('feedback-form-state');
+const parsed = JSON.parse(getStorageObject);
+
+if (parsed.email !== undefined) {
+  email.value = parsed.email;
+}
+if (parsed.message !== undefined) {
+  message.value = parsed.message;
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  console.log(
+    'This is current data from Local Storage',
+    JSON.parse(getStorageObject)
+  );
+
+  localStorage.removeItem('feedback-form-state');
+
+  const currentData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  console.log('This is current data from form', currentData);
+
+  email.value = '';
+  message.value = '';
+});
