3f5fd8e4d31fc7725ea17aa9fc55b7d4ab81c02d
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-11 20:26:47 +0300

    fixes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,25 @@
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 //Refs
 const { gallery } = {
-  gallery: document.body.querySelector(".gallery"),
+  gallery: document.body.querySelector('.gallery'),
 };
 
 //Creating markup
-galleryItems.forEach((item) => {
-  gallery.insertAdjacentHTML(
-    "beforeend",
-    `<a class="gallery__item" href="${item.original}">
+const markup = galleryItems.map(item => {
+  return `<a class="gallery__item" href="${item.original}">
   <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="test" />
-</a>`
-  );
+</a>`;
 });
 
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+
 //Initialize SimpleLightBox
-var lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
 });
